9d62aa1254d2bf2fa81ec3cb975657a5a4ac31e2
siddh-dw <siddh03@gmail.com>
2022-04-24 00:20:54 -0500

    24-04-22_commit2

--- run.py
+++ run.py
@@ -1,17 +1,24 @@
-from flask import Flask
+import re
+from flask import Flask, render_template
 app = Flask(__name__)
 
 posts = []
-# @app.route()
-# def index():
-#     return "{} posts".format(len(posts))
+@app.route("/")
+def index():
+    return render_template("index.html", num_post=len(posts))
 
-# @app.route("/p/<string:slug>/")
-# def show_post(slug):
-#     return "Mostrando el post{}".format(slug)
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return render_template("post_view.html", slug_title=slug)
 
 @app.route("/admin/post/")
 @app.route("/admin/post/<int:post_id>/")
 def post_form(post_id=None):
-    return "post_form{}".format(post_id)
+    return render_template("admin/post_form.html",post_id=post_id)
+
+@app.route("/signalup")
+def show_signup_form():
+    return render_template("signup_form.html")
+
+
 
